398636086caa00f4a01050168e445a4aea9a2fa3
Edemilson da mata <edemilsondell@hotmail.com>
2020-04-24 22:40:07 -0400

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,13 +2,18 @@ package application;
 
 import java.io.IOException;
 
+import gui.util.Alerts;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -17,18 +22,23 @@ public class Main extends Application {
 			ScrollPane scrollPane = loader.load();
 			
 			scrollPane.setFitToHeight(true); 
-			scrollPane.setFitToWidth(true); 
+			scrollPane.setFitToWidth(true); 			
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
 		} catch (IOException e) {
-			e.printStackTrace();
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+			
 		}
 	}
 
+	public static Scene getMainScene() {		
+		return mainScene;
+	}
 	public static void main(String[] args) {
 		launch(args);
 	}
+		
 }
